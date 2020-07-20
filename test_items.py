6bb4f9aac7af89bb0c843423e5d001357a2d2492
abrgzej <dantumonskiy@gmail.com>
2020-07-20 16:32:11 +0300

    + Translate test_items.py

--- test_items.py
+++ test_items.py
@@ -2,4 +2,4 @@ def test_add_to_basket(request, browser):
     language = request.config.getoption("language")
     browser.get(f'http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/')
     elements = browser.find_elements_by_css_selector('.btn-add-to-basket')
-    assert len(elements) == 1, 'Количество элементов на странице не равно одному'
+    assert len(elements) == 1, 'The number of elements per page is not equal to one'
